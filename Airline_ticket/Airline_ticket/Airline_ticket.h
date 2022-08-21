2e0a5072f37bcedede3e0cae6119a174506a8e2c
LiubovPil <alexandraelle95@gmail.com>
2022-08-21 19:34:02 +0300

    Add airline_ticket

---
+++ Airline_ticket/Airline_ticket/Airline_ticket.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+struct Date {
+	int year, month, day;
+};
+
+struct Time {
+	int hours, minutes;
+};
+
+struct AirlineTicket {
+	string from;
+	string to;
+	string airline;
+	Date departure_date;
+	Time departure_time;
+	Date arrival_date;
+	Time arrival_time;
+	int price;
+};
+
+bool operator < (const Date& lhs, const Date& rhs); 
+
+bool operator == (const Date& lhs, const Date& rhs); 
+
+ostream& operator << (ostream& out, const Date& date);
+
+bool operator < (const Time& lhs, const Time& rhs); 
+
+bool operator == (const Time& lhs, const Time& rhs); 
+
+ostream& operator << (ostream& out, const Time& time);
