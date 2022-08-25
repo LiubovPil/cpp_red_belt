bafd689b408a05f3407ce3493db43d0fe8115a32
LiubovPil <alexandraelle95@gmail.com>
2022-08-25 18:06:54 +0300

    Checking

--- Paginator/Paginator/Paginator.cpp
+++ Paginator/Paginator/Paginator.cpp
@@ -15,10 +15,12 @@ template <typename Iterator>
 class IteratorRange {
 private:
     Iterator first, last;
+    size_t size_;
 public:
     IteratorRange(Iterator f, Iterator l)
         : first(f)
         , last(l)
+        , size_(size())
     {
     }
     Iterator begin() const {
@@ -34,7 +36,8 @@ public:
 
 template <typename Iterator>
 class Paginator {
-public:
+
+/*public:
     Paginator(Iterator first, Iterator end, size_t page_size){
         Iterator border = first;
         for (size_t i = 0; i < page_size; i++) {
@@ -45,22 +48,48 @@ public:
             border = next(border, page_size);
         }
     }
-    Iterator begin() const {
+    auto begin() const {
         return pages.begin();
     }
-    Iterator end() const {
+    auto end() const {
         return pages.end();
     }
     size_t size() const {
         return pages.size();
     }
 private:
-    vector<vector<Iterator>> pages;
+    vector<vector<Iterator>> pages;*/
+private:
+    vector<IteratorRange<Iterator>> pages;
+
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (size_t left = distance(begin, end); left > 0; ) {
+            size_t current_page_size = min(page_size, left);
+            Iterator current_page_end = next(begin, current_page_size);
+            pages.push_back({ begin, current_page_end });
+
+            left -= current_page_size;
+            begin = current_page_end;
+        }
+    }
+
+    auto begin() const {
+        return pages.begin();
+    }
+
+    auto end() const {
+        return pages.end();
+    }
+
+    size_t size() const {
+        return pages.size();
+    }
 };
 
 template <typename C>
-auto Paginate(C & c, size_t page_size) {
-    return Paginator((begin(c)), (end(c)), page_size);
+auto Paginate(C& c, size_t page_size) {
+    return Paginator((c.begin()), (c.end()), page_size);
 }
 
 void TestPageCounts() {
