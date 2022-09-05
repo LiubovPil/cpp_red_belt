0d9d95606d87309e1962a3f7aa7de48d540ec916
LiubovPil <alexandraelle95@gmail.com>
2022-09-05 21:08:26 +0300

    Adding project + swap and sort

---
+++ Swap_Sort_Reversed/Swap_Sort_Reversed/Swap_Sort_Reversed.cpp
@@ -0,0 +1,96 @@
+﻿// Tasks from the course "Fundamentals of C++: Red Belt
+// Done by Liubov Piliagina
+
+#include "Test_runner.h"
+
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+template <typename T>
+void Swap(T* first, T* second) {
+    auto temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+template <typename T>
+void SortPointers(vector<T*>& pointers) {
+    /*vector<T> values;
+    for (auto p : pointers)
+        values.push_back(*p);
+
+    sort(values.begin(), values.end());
+    for (size_t i = 0; i < values.size(); ++i)
+        pointers[i] = &values[i];*/
+    sort(begin(pointers), end(pointers), [](T* lhs, T* rhs) {
+        return *lhs < *rhs;
+        });
+}
+
+template <typename T>
+void ReversedCopy(T* source, size_t count, T* destination);
+
+void TestSwap() {
+    int a = 1;
+    int b = 2;
+    Swap(&a, &b);
+    ASSERT_EQUAL(a, 2);
+    ASSERT_EQUAL(b, 1);
+
+    string h = "world";
+    string w = "hello";
+    Swap(&h, &w);
+    ASSERT_EQUAL(h, "hello");
+    ASSERT_EQUAL(w, "world");
+}
+
+void TestSortPointers() {
+    int one = 1;
+    int two = 2;
+    int three = 3;
+
+    vector<int*> pointers;
+    pointers.push_back(&two);
+    pointers.push_back(&three);
+    pointers.push_back(&one);
+
+    SortPointers(pointers);
+
+    ASSERT_EQUAL(pointers.size(), 3u);
+    ASSERT_EQUAL(*pointers[0], 1);
+    ASSERT_EQUAL(*pointers[1], 2);
+    ASSERT_EQUAL(*pointers[2], 3);
+}
+
+void TestReverseCopy() {
+    const size_t count = 7;
+
+    int* source = new int[count];
+    int* dest = new int[count];
+
+    for (size_t i = 0; i < count; ++i) {
+        source[i] = i + 1;
+    }
+    ReversedCopy(source, count, dest);
+    const vector<int> expected1 = { 7, 6, 5, 4, 3, 2, 1 };
+    ASSERT_EQUAL(vector<int>(dest, dest + count), expected1);
+
+    // Области памяти могут перекрываться
+    ReversedCopy(source, count - 1, source + 1);
+    const vector<int> expected2 = { 1, 6, 5, 4, 3, 2, 1 };
+    ASSERT_EQUAL(vector<int>(source, source + count), expected2);
+
+    delete[] dest;
+    delete[] source;
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestSwap);
+    RUN_TEST(tr, TestSortPointers);
+    RUN_TEST(tr, TestReverseCopy);
+    return 0;
+}
+
+
---
+++ Swap_Sort_Reversed/Swap_Sort_Reversed/Test_runner.cpp
@@ -0,0 +1,8 @@
+#include "Test_runner.h"
+
+TestRunner:: ~TestRunner() {
+	if (fail_count > 0) {
+		cerr << fail_count << " unit tests failed. Terminate" << endl;
+		exit(1);
+	}
+}
