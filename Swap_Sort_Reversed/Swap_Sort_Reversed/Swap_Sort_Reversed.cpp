f0c59034dc8737fcaa766701e41b0d15243c060b
LiubovPil <alexandraelle95@gmail.com>
2022-09-06 14:36:55 +0300

    Adding reverse, finish

--- Swap_Sort_Reversed/Swap_Sort_Reversed/Swap_Sort_Reversed.cpp
+++ Swap_Sort_Reversed/Swap_Sort_Reversed/Swap_Sort_Reversed.cpp
@@ -16,20 +16,32 @@ void Swap(T* first, T* second) {
 
 template <typename T>
 void SortPointers(vector<T*>& pointers) {
-    /*vector<T> values;
-    for (auto p : pointers)
-        values.push_back(*p);
-
-    sort(values.begin(), values.end());
-    for (size_t i = 0; i < values.size(); ++i)
-        pointers[i] = &values[i];*/
     sort(begin(pointers), end(pointers), [](T* lhs, T* rhs) {
         return *lhs < *rhs;
         });
 }
 
 template <typename T>
-void ReversedCopy(T* source, size_t count, T* destination);
+void ReversedCopy(T* source, size_t count, T* destination) {
+    auto source_begin = source;
+    auto source_end = source + count;
+    auto dest_begin = destination;
+    auto dest_end = destination + count;
+    if(dest_begin >= source_end || dest_end <= source_begin)
+        reverse_copy(source_begin, source_end, dest_begin);
+    else if (dest_begin > source_begin) {
+        for (size_t i = 0; source_begin + i < dest_begin; ++i) {
+            *(dest_end - i - 1) = *(source_begin + i);
+        }
+        reverse(dest_begin, source_end);
+    }
+    else {
+        for (size_t i = 0; source_end - i - 1 >= dest_end; ++i) {
+            *(dest_begin + i) = *(source_end - i - 1);
+        }
+        reverse(source_begin, dest_end);
+    }
+}
 
 void TestSwap() {
     int a = 1;
