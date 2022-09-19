a5e3180d7c1873f963c5b8fb921de85cf7e446e6
LiubovPil <alexandraelle95@gmail.com>
2022-09-19 14:51:07 +0300

    Finish ServerStats

--- ServerStats/ServerStats/Stats.cpp
+++ ServerStats/ServerStats/Stats.cpp
@@ -1,18 +1,27 @@
 #include "Stats.h"
 #include <cctype>
 
+void StatUnit::Add(string_view value) {
+	if (auto it = counts.find(value); it != counts.end()) {
+		++it->second;
+	}
+	else {
+		++counts[default_key];
+	}
+}
 void Stats::AddMethod(string_view method){
-	
+	method_stats.Add(method);
 }
 void Stats::AddUri(string_view uri) {
-
+	uri_stats.Add(uri);
 }
 const map<string_view, int>& Stats::GetMethodStats() const {
-
+	return method_stats.GetValues();
 }
 const map<string_view, int>& Stats::GetUriStats() const {
-
+	return uri_stats.GetValues();
 }
+
 void RemoveSpace(string_view& sv) {
 	while (!sv.empty() && isspace(sv[0])) {
 		sv.remove_prefix(1);
@@ -20,7 +29,6 @@ void RemoveSpace(string_view& sv) {
 }
 string_view ReadToken(string_view& sv) {
 	RemoveSpace(sv);
-
 	auto pos = sv.find(' ');
 	auto result = sv.substr(0, pos);
 	sv.remove_prefix(pos != sv.npos ? pos : sv.size());
--- ServerStats/ServerStats/Stats.h
+++ ServerStats/ServerStats/Stats.h
@@ -7,6 +7,28 @@
 #include <map>
 using namespace std;
 
+class StatUnit {
+public:
+	template <typename Container>
+	StatUnit(const Container& known_keys, string_view default_key)
+		: default_key(default_key)
+	{
+		counts[default_key] = 0;
+		for (string_view key : known_keys) {
+			counts[key] = 0;
+		}
+	}
+
+	void Add(string_view value);
+
+	const map<string_view, int>& GetValues() const {
+		return counts;
+	}
+private:
+	const string_view default_key;
+	map<string_view, int> counts;
+};
+
 class Stats {
 public:
 	Stats() = default;
@@ -17,11 +39,14 @@ public:
 	const map<string_view, int>& GetUriStats() const;
 
 private:
-	inline static const array<string, 5> methods_name = { "GET" , "PUT" , "POST" , "DELETE" , "UNKNOWN" };
-	inline static const array<string, 6> uri_name = { "/", "/order" , "/product" , "/basket" , "/help" , "unknown" };
+	inline static const array<string, 4> methods_name = { "GET" , "PUT" , "POST" , "DELETE" };
+	inline static const string default_method_name = "UNKNOWN";
 
-	inline static const map<string_view, int> method_stats;
-	inline static const map<string_view, int> uri_stats;
+	inline static const array<string, 5> uri_name = { "/", "/order" , "/product" , "/basket" , "/help"};
+	inline static const string default_uri_name = "unknown";
+
+	StatUnit method_stats{ methods_name, default_method_name };
+	StatUnit uri_stats{ uri_name, default_uri_name };
 };
 
 HttpRequest ParseRequest(string_view line);
